14788fd7454a5a5977e09f13b9116c2a418f4fbd
marc <marc@urbanairship.com>
2012-11-19 17:08:07 -0800

    do git archive in the build script and source distribution from there

--- build.sh
+++ build.sh
@@ -1,27 +1,30 @@
-#Do this from a fresh checkout so there are no generated files.
+#!/bin/bash
 
 VERSION="1.0.2"
 
+mkdir -p archive
 mkdir -p build/android
 mkdir -p build/ios
 
+git archive master | tar -xC archive/
+
 #Copy the README, CHANGELOG and COPYING
-cp README.md build/README
-cp COPYING build
-cp CHANGELOG build
+cp archive/README.md build/README
+cp archive/COPYING build
+cp archive/CHANGELOG build
 
 #Copy the sample apps
-cp -r android-sample build/android/sample-app
-cp -r ios-sample build/ios/sample-app
+cp -r archive/android-sample build/android/sample-app
+cp -r archive/ios-sample build/ios/sample-app
 
 #Copy android files to android dir
-cp android-sample/assets/www/lib/PushNotification.js build/android/
-cp android-sample/src/com/urbanairship/phonegap/plugins/PushNotificationPlugin.java build/android/
-cp android-sample/src/com/urbanairship/phonegap/sample/IntentReceiver.java build/android/
+cp archive/android-sample/assets/www/lib/PushNotification.js build/android/
+cp archive/android-sample/src/com/urbanairship/phonegap/plugins/PushNotificationPlugin.java build/android/
+cp archive/android-sample/src/com/urbanairship/phonegap/sample/IntentReceiver.java build/android/
 
 #Copy IOS files to ios dir.
-cp ios-sample/www/lib/PushNotification.js build/ios/
-cp -r ios-sample/UAPhonegapSample/Plugins/PushNotificationPlugin build/ios/
+cp archive/ios-sample/www/lib/PushNotification.js build/ios/
+cp -r archive/ios-sample/UAPhonegapSample/Plugins/PushNotificationPlugin build/ios/
 
 #Build release
 BUILD_DIR="urbanairship-phonegap-$VERSION"
@@ -31,3 +34,4 @@ rm $BUILD_DIR
 
 #Remove build dir.
 rm -rf build
+rm -rf archive
