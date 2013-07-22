52099a8b06305d9e5f2296fb3e94042ce7cbb8ad
Ryan Lepinski <ryan.lepinski@urbanairship.com>
2013-07-22 15:08:42 -0700

    Look for config in the config.xml file instead of the manifest

--- src/android/PushAutopilot.java
+++ src/android/PushAutopilot.java
@@ -1,9 +1,7 @@
 package com.urbanairship.phonegap;
 
 import android.app.Application;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
-import android.os.Bundle;
+import android.content.res.XmlResourceParser;
 
 import com.urbanairship.AirshipConfigOptions;
 import com.urbanairship.Autopilot;
@@ -11,19 +9,22 @@ import com.urbanairship.Logger;
 import com.urbanairship.UAirship;
 import com.urbanairship.push.PushManager;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PushAutopilot extends Autopilot {
+    static final String UA_PREFIX = "com.urbanairship";
     static final String PRODUCTION_KEY = "com.urbanairship.production_app_key";
     static final String PRODUCTION_SECRET = "com.urbanairship.production_app_secret";
     static final String DEVELOPMENT_KEY = "com.urbanairship.development_app_key";
     static final String DEVELOPMENT_SECRET = "com.urbanairship.development_app_secret";
     static final String IN_PRODUCTION = "com.urbanairship.in_production";
-    static final String GCM_SENDER = "com.urbanairship.gcm_sender";
+    static final String GCM_SENDER = "com.urbanairship.gcm_sender_id";
     static final String LOCATION_ENABLED = "com.urbanairship.location_enabled";
-
 
     @Override
     public void execute(Application application) {
-        UAirship.takeOff(application, gatherConfigOptions(application));
+        UAirship.takeOff(application, getAirshipConfig(application));
 
         PushManager.shared().setIntentReceiver(PushReceiver.class);
         if (UAirship.shared().getAirshipConfigOptions().pushServiceEnabled) {
@@ -32,36 +33,71 @@ public class PushAutopilot extends Autopilot {
     }
 
 
-    private AirshipConfigOptions gatherConfigOptions(Application application) {
-        // Create the default options
+    private AirshipConfigOptions getAirshipConfig(Application application) {
+        // Create the default options, will pull any config from the usual place - assets/airshipconfig.properties
         AirshipConfigOptions options = AirshipConfigOptions.loadDefaultOptions(application);
 
-        //Find any overrides in the manifest
-        Bundle metaData = getMetaDataBundle(application);
-        if (metaData == null) {
-            return options;
-        }
+        //Find any overrides in the cordova config
+        AirshipOptions configOptions = new AirshipOptions(application);
 
         // Apply any overrides from the manifest
-        options.productionAppKey = metaData.getString(PRODUCTION_KEY, options.productionAppKey);
-        options.productionAppSecret = metaData.getString(PRODUCTION_SECRET, options.productionAppSecret);
-        options.developmentAppKey = metaData.getString(DEVELOPMENT_KEY, options.developmentAppKey);
-        options.developmentAppSecret = metaData.getString(DEVELOPMENT_SECRET, options.productionAppKey);
-        options.inProduction = metaData.getBoolean(IN_PRODUCTION, options.inProduction);
-        options.gcmSender = metaData.getString(GCM_SENDER, options.gcmSender);
-        options.locationOptions.locationServiceEnabled = metaData.getBoolean(LOCATION_ENABLED, options.locationOptions.locationServiceEnabled);
-        return options;
+        options.productionAppKey = configOptions.getString(PRODUCTION_KEY, options.productionAppKey);
+        options.productionAppSecret = configOptions.getString(PRODUCTION_SECRET, options.productionAppSecret);
+        options.developmentAppKey = configOptions.getString(DEVELOPMENT_KEY, options.developmentAppKey);
+        options.developmentAppSecret = configOptions.getString(DEVELOPMENT_SECRET, options.productionAppKey);
+        options.gcmSender = configOptions.getString(GCM_SENDER, options.gcmSender);
+        options.inProduction = configOptions.getBoolean(IN_PRODUCTION, options.inProduction);
+        options.locationOptions.locationServiceEnabled = configOptions.getBoolean(LOCATION_ENABLED, options.locationOptions.locationServiceEnabled);
 
+        return options;
     }
 
+    class AirshipOptions {
+        private Map<String, String> airshipValues = new HashMap<String, String>();
 
-    private Bundle getMetaDataBundle(Application application) {
-        try {
-            ApplicationInfo ai = application.getPackageManager().getApplicationInfo(application.getPackageName(), PackageManager.GET_META_DATA);
-            return ai.metaData;
-        } catch (Exception ex) {
-            Logger.error("Unable to get package info", ex);
+        AirshipOptions(Application application) {
+            parseConfig(application);
         }
-        return null;
+
+        String getString(String key, String defaultValue) {
+            return airshipValues.containsKey(key) ? airshipValues.get(key) : defaultValue;
+        }
+
+        boolean getBoolean(String key, boolean defaultValue) {
+            return airshipValues.containsKey(key) ?
+                    Boolean.parseBoolean(airshipValues.get(key)) : defaultValue;
+        }
+
+        private void parseConfig(Application application) {
+            int id = application.getResources().getIdentifier("config", "xml", application.getPackageName());
+            if (id == 0) {
+                return;
+            }
+
+            XmlResourceParser xml = application.getResources().getXml(id);
+
+            int eventType = -1;
+            while (eventType != XmlResourceParser.END_DOCUMENT) {
+
+                if (eventType == XmlResourceParser.START_TAG) {
+                    if (xml.getName().equals("preference")) {
+                        String name = xml.getAttributeValue(null, "name").toLowerCase();
+                        String value = xml.getAttributeValue(null, "value");
+
+                        if (name.startsWith(UA_PREFIX) && value != null) {
+                            airshipValues.put(name, value);
+                            Logger.verbose("Found " + name + " in config.xml with value: " + value);
+                        }
+                    }
+                }
+
+                try {
+                    eventType = xml.next();
+                } catch (Exception e) {
+                    Logger.error("Error parsing config file", e);
+                }
+            }
+        }
+
     }
 }
