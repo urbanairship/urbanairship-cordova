b56c4a4efd80a471cb0c2233fb3da6e8a37aa75f
Ryan Lepinski <ryan.lepinski@urbanairship.com>
2013-12-30 09:16:36 -0800

    Dispatch to main queue

--- src/ios/PushNotificationPlugin.h
+++ src/ios/PushNotificationPlugin.h
@@ -1,17 +1,32 @@
-//
-//  UrbanAirshipPlugin.h
-//  urbanairship.richpush
-//
-//  Created by urbanairship on 6/18/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
+/*
+ Copyright 2009-2013 Urban Airship Inc. All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer.
+
+ 2. Redistributions in binaryform must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided withthe distribution.
+
+ THIS SOFTWARE IS PROVIDED BY THE URBAN AIRSHIP INC``AS IS'' AND ANY EXPRESS OR
+ IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ EVENT SHALL URBAN AIRSHIP INC OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #import <Foundation/Foundation.h>
 #import <Cordova/CDVPlugin.h>
 #import "UAPush.h"
 
-@interface PushNotificationPlugin : CDVPlugin <UARegistrationObserver, UAPushNotificationDelegate> {
-    dispatch_queue_t dispatchQueue;
-}
+@interface PushNotificationPlugin : CDVPlugin <UARegistrationObserver, UAPushNotificationDelegate>
 
 @end
--- src/ios/PushNotificationPlugin.m
+++ src/ios/PushNotificationPlugin.m
@@ -1,3 +1,28 @@
+/*
+ Copyright 2009-2013 Urban Airship Inc. All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer.
+
+ 2. Redistributions in binaryform must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided withthe distribution.
+
+ THIS SOFTWARE IS PROVIDED BY THE URBAN AIRSHIP INC``AS IS'' AND ANY EXPRESS OR
+ IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ EVENT SHALL URBAN AIRSHIP INC OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #import "PushNotificationPlugin.h"
 #import "UAPush.h"
 #import "UAirship.h"
@@ -18,7 +43,6 @@ typedef void (^UACordovaVoidCallbackBlock)(NSArray *args);
 
 - (void)pluginInitialize {
     UA_LINFO("Initializing PushNotificationPlugin");
-    dispatchQueue =  dispatch_queue_create("com.urbanairship.push_plugin", DISPATCH_QUEUE_SERIAL);
     [self takeOff];
 }
 
@@ -32,7 +56,6 @@ typedef void (^UACordovaVoidCallbackBlock)(NSArray *args);
     config.productionAppSecret = [settings valueForKey:@"com.urbanairship.production_app_secret"] ?: config.productionAppSecret;
     config.developmentAppKey = [settings valueForKey:@"com.urbanairship.development_app_key"] ?: config.developmentAppKey;
     config.developmentAppSecret = [settings valueForKey:@"com.urbanairship.development_app_secret"] ?: config.developmentAppSecret;
-
     if ([settings valueForKey:@"com.urbanairship.in_production"]) {
         config.inProduction = [[settings valueForKey:@"com.urbanairship.in_production"] boolValue];
     }
@@ -112,7 +135,7 @@ typedef void (^UACordovaVoidCallbackBlock)(NSArray *args);
 
 - (void)performCallbackWithCommand:(CDVInvokedUrlCommand*)command expecting:(NSArray *)expected withBlock:(UACordovaCallbackBlock)block {
 
-    dispatch_async(dispatchQueue, ^{
+    dispatch_async(dispatch_get_main_queue(), ^{
         //if we're expecting any arguments
         if (expected) {
             if (![self validateArguments:command.arguments forExpectedTypes:expected]) {
@@ -143,6 +166,8 @@ typedef void (^UACordovaVoidCallbackBlock)(NSArray *args);
         return [NSNull null];
     }];
 }
+
+
 
 - (NSString *)alertForUserInfo:(NSDictionary *)userInfo {
     NSString *alert = @"";
