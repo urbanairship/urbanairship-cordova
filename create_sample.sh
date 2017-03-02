514d244f2434cb4f6bab169d67c9459b6200cd7e
hcrowell <helen.crowell@urbanairship.com>
2017-03-02 13:07:27 -0800

    [MB-2125] Updated scripts

--- create_sample.sh
+++ create_sample.sh
@@ -1,44 +1,55 @@
 #!/bin/bash -ex
+ROOT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-# Set up script for iOS and android testing.
+TEST_PATH=$1
+
+if [ -z "$1" ]
+  then
+    echo "No test path supplied"
+    exit
+fi
+
+# Set up script to create a sample for iOS and android testing.
 # Follow the steps below.
 # 1. Add the UA credentials to the `config_sample.xml` file in the test directory and save.
-# 2. Run the script with the command `./test_sample.sh `
+# 2. Run the script with the command `./create_sample.sh `
 # 3. Build the platform you want to test (see comments below).
 
 # keep cordova up to date
 npm install cordova -g
 
-# remove any previous test
-rm -rf test
+# create test directory
+mkdir $TEST_PATH
+cd $TEST_PATH
 
 # create the test project
 cordova create test com.urbanairship.sample Test
-
 cd test
 
+TEST_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
 # add the plugin
-cordova plugin add ../../
+cordova plugin add $ROOT_DIR
 
 # copy config and example files
-cp ../config_sample.xml config.xml
-cp ../../Example/index.html www/index.html
-cp ../../Example/css/* www/css
-cp ../../Example/js/* www/js
+cp $ROOT_DIR/config_sample.xml config.xml
+cp $ROOT_DIR/Example/index.html www/index.html
+cp $ROOT_DIR/Example/css/* www/css
+cp $ROOT_DIR/Example/js/* www/js
 
 # add the device plugin
 cordova plugin add cordova-plugin-device
 
 # set up iOS
 cordova platform add ios
-cd platforms/ios/
+cd $TEST_DIR/platforms/ios/
 pod update
-cd ../..
 # Build with command `cordova build ios` in test directory
 # After successful build, connect iOS device to test
 # Open workspace to test with command `open platforms/ios/Test.xcworkspace`
 
 # set up android
+cd $TEST_DIR
 cordova platform add android
 # Build with command `cordova build android` in test directory
 # After successful build, connect android device to test
