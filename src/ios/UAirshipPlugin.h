10d52288d75c61fa7fed8a16cbec2fc82824a59b
Ryan Lepinski <ryan.lepinski@urbanairship.com>
2018-06-29 11:07:21 -0700

    [MOBILE-215] Enable takeOff from JS for iOS (#236)

--- src/ios/UAirshipPlugin.h
+++ src/ios/UAirshipPlugin.h
@@ -1,27 +1,4 @@
-/*
- Copyright 2009-2017 Urban Airship Inc. All rights reserved.
-
- Redistribution and use in source and binary forms, with or without
- modification, are permitted provided that the following conditions are met:
-
- 1. Redistributions of source code must retain the above copyright notice, this
- list of conditions and the following disclaimer.
-
- 2. Redistributions in binary form must reproduce the above copyright notice,
- this list of conditions and the following disclaimer in the documentation
- and/or other materials provided with the distribution.
-
- THIS SOFTWARE IS PROVIDED BY THE URBAN AIRSHIP INC ``AS IS'' AND ANY EXPRESS OR
- IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
- EVENT SHALL URBAN AIRSHIP INC OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
- OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
- ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+/* Copyright 2018 Urban Airship and Contributors */
 
 #import <Foundation/Foundation.h>
 #import <Cordova/CDVPlugin.h>
@@ -30,7 +7,26 @@
 /**
  * The Urban Airship Cordova plugin.
  */
-@interface UAirshipPlugin : CDVPlugin <UARegistrationDelegate, UAPushNotificationDelegate, UAInboxDelegate>
+@interface UAirshipPlugin : CDVPlugin
+
+/**
+ * Sets the Urban Airship config and attempts takeOff.
+ *
+ * Expected arguments: NSDictionary
+ *
+ * @param command The cordova command.
+ */
+- (void)takeOff:(CDVInvokedUrlCommand *)command;
+
+/**
+ * Sets the default behavior when the message center is launched from a push
+ * notification. If set to false the message center must be manually launched.
+ *
+ * Expected arguments: Boolean
+ *
+ * @param command The cordova command.
+ */
+- (void)setAutoLaunchDefaultMessageCenter:(CDVInvokedUrlCommand *)command;
 
 /**
  * Enables or disables user push notifications.
