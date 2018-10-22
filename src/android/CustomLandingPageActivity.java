d0ca2d77ceed74120636ba80a699b4e2fff422c3
Ryan Lepinski <ryan.lepinski@urbanairship.com>
2018-10-22 14:44:07 -0700

    [MOBILE-361] Enable local storage flag

--- src/android/CustomLandingPageActivity.java
+++ src/android/CustomLandingPageActivity.java
@@ -13,27 +13,13 @@ public class CustomLandingPageActivity extends LandingPageActivity {
     @Override
     public void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-
         if (getIntent() != null && "CANCEL".equals(getIntent().getAction())) {
             finish();
         }
-
-        /*
-         * From http://developer.android.com/reference/android/webkit/WebSettings.html#setDatabaseEnabled(boolean):
-         * "This setting is global in effect, across all WebView instances in a process. Note you should only
-         *  modify this setting prior to making any WebView page load within a given process, as the WebView
-         *  implementation may ignore changes to this setting after that point."
-         *
-         * So in order to not break applications that depend on the web store APIs, we must enable
-         * this setting for our landing page in case it is the first web view to load content.
-         */
-        UAWebView webView = (UAWebView) findViewById(android.R.id.primary);
-        webView.getSettings().setDatabaseEnabled(true);
     }
 
     @Override
     public void onNewIntent(Intent intent) {
-
         if (intent != null && "CANCEL".equals(intent.getAction())) {
             finish();
             return;
