39676741d0f51fe39c3288ed4ac6d1d2f19af71a
Ryan Lepinski <ryan.lepinski@urbanairship.com>
2019-02-04 15:36:20 -0800

    [GH-286] Fix crash when user notifications disable

--- src/android/events/PushEvent.java
+++ src/android/events/PushEvent.java
@@ -2,6 +2,10 @@
 
 package com.urbanairship.cordova.events;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.urbanairship.cordova.Utils;
 import com.urbanairship.Logger;
 import com.urbanairship.push.PushMessage;
 import com.urbanairship.util.UAStringUtil;
@@ -21,63 +25,26 @@ public class PushEvent implements Event {
 
     private final PushMessage message;
     private final Integer notificationId;
-    private String notificationTag;
 
-    public PushEvent(Integer notificationId, PushMessage message) {
+    public PushEvent(@Nullable Integer notificationId, @NonNull PushMessage message) {
         this.notificationId = notificationId;
         this.message = message;
     }
 
-    public PushEvent(Integer notificationId, PushMessage message, String tag) {
-        this.notificationId = notificationId;
-        this.notificationTag = tag;
-        this.message = message;
-    }
-
+    @NonNull
     @Override
     public String getEventName() {
         return EVENT_PUSH_RECEIVED;
     }
 
+    @Nullable
     @Override
     public JSONObject getEventData() {
-        JSONObject data = new JSONObject();
-
-        Map<String, String> extras = new HashMap<String, String>();
-        for (String key : message.getPushBundle().keySet()) {
-            if ("android.support.content.wakelockid".equals(key)) {
-                continue;
-            }
-            if ("google.sent_time".equals(key)) {
-                extras.put(key, Long.toString(message.getPushBundle().getLong(key)));
-                continue;
-            }
-            if ("google.ttl".equals(key)) {
-                extras.put(key, Integer.toString(message.getPushBundle().getInt(key)));
-                continue;
-            }
-            extras.put(key, message.getPushBundle().getString(key));
-        }
-
         try {
-            data.putOpt("message", message.getAlert());
-            data.putOpt("extras", new JSONObject(extras));
-            data.putOpt("notification_id", notificationId);
-            data.putOpt("notificationId", getNotificationdId(notificationId, notificationTag));
+            return Utils.notificationObject(message, message.getNotificationTag(), notificationId);
         } catch (JSONException e) {
             Logger.error("Error constructing notification object", e);
+            return null;
         }
-
-        return data;
     }
-
-    static String getNotificationdId(int notificationId, String notificationTag) {
-        String id = String.valueOf(notificationId);
-        if (!UAStringUtil.isEmpty(notificationTag)) {
-            id += ":" + notificationTag;
-        }
-
-        return id;
-    }
-
 }
