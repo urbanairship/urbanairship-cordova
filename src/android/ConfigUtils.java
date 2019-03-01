9704050401e6daec52c6f91953d96a2db392de05
Christopher Weight <chris@chrisweight.com>
2019-03-01 10:20:10 +0000

    Fixes whitespace issues

--- src/android/ConfigUtils.java
+++ src/android/ConfigUtils.java
@@ -16,93 +16,93 @@ import java.util.Map;
  * Config Utils.
  */
 class ConfigUtils {
-    private static final String UA_PREFIX = "com.urbanairship";
-    private static final String SENDER_PREFIX = "sender:";
+  private static final String UA_PREFIX = "com.urbanairship";
+  private static final String SENDER_PREFIX = "sender:";
 
-    /**
-     * Convert the log level string to an int.
-     *
-     * @param logLevel        The log level as a string.
-     * @param defaultLogLevel Default log level.
-     * @return The log level.
-     */
-    public static int parseLogLevel(@Nullable String logLevel, int defaultLogLevel) {
-        if (logLevel == null || logLevel.length() == 0) {
-            return defaultLogLevel;
-        }
-        String logString = logLevel.trim().toLowerCase();
-				if (logString.equals("verbose")) {
-					return Log.VERBOSE;
-				} else if (logString.equals("debug")) {
-					return Log.DEBUG;
-				} else if (logString.equals("info")) {
-					return Log.INFO;
-				} else if (logString.equals("warn")) {
-					return Log.WARN;
-				} else if (logString.equals("error")) {
-					return Log.ERROR;
-				} else if (logString.equals("none")) {
-					return Log.ASSERT;
-				}
-				return defaultLogLevel;
+  /**
+   * Convert the log level string to an int.
+   *
+   * @param logLevel        The log level as a string.
+   * @param defaultLogLevel Default log level.
+   * @return The log level.
+   */
+  public static int parseLogLevel(@Nullable String logLevel, int defaultLogLevel) {
+    if (logLevel == null || logLevel.length() == 0) {
+      return defaultLogLevel;
+    }
+    String logString = logLevel.trim().toLowerCase();
+    if (logString.equals("verbose")) {
+      return Log.VERBOSE;
+    } else if (logString.equals("debug")) {
+      return Log.DEBUG;
+    } else if (logString.equals("info")) {
+      return Log.INFO;
+    } else if (logString.equals("warn")) {
+      return Log.WARN;
+    } else if (logString.equals("error")) {
+      return Log.ERROR;
+    } else if (logString.equals("none")) {
+      return Log.ASSERT;
+    }
+    return defaultLogLevel;
+  }
+
+  /**
+   * Parses the sender ID.
+   *
+   * @param value The value from config.
+   * @return The sender ID.
+   */
+  @Nullable
+  public static String parseSender(@Nullable String value) {
+    if (value == null) {
+      return null;
     }
 
-    /**
-     * Parses the sender ID.
-     *
-     * @param value The value from config.
-     * @return The sender ID.
-     */
-    @Nullable
-    public static String parseSender(@Nullable String value) {
-        if (value == null) {
-            return null;
-        }
-
-        if (value.startsWith("sender:")) {
-            return value.substring(SENDER_PREFIX.length());
-        }
-
-        return value;
+    if (value.startsWith("sender:")) {
+      return value.substring(SENDER_PREFIX.length());
     }
 
-    /**
-     * Parses the config.xml file for any Urban Airship config.
-     *
-     * @param context The application context.
-     */
-    @NonNull
-    public static Map<String, String> parseConfigXml(@NonNull Context context) {
-        Map<String, String> config = new HashMap<String, String>();
-        int id = context.getResources().getIdentifier("config", "xml", context.getPackageName());
-        if (id == 0) {
-            return config;
-        }
+    return value;
+  }
 
-        XmlResourceParser xml = context.getResources().getXml(id);
-
-        int eventType = -1;
-        while (eventType != XmlResourceParser.END_DOCUMENT) {
-
-            if (eventType == XmlResourceParser.START_TAG) {
-                if (xml.getName().equals("preference")) {
-                    String name = xml.getAttributeValue(null, "name").toLowerCase(Locale.US);
-                    String value = xml.getAttributeValue(null, "value");
-
-                    if (name.startsWith(UA_PREFIX) && value != null) {
-                        config.put(name, value);
-                        PluginLogger.verbose("Found %s in config.xml with value: %s", name, value);
-                    }
-                }
-            }
-
-            try {
-                eventType = xml.next();
-            } catch (Exception e) {
-                PluginLogger.error(e, "Error parsing config file");
-            }
-        }
-
-        return config;
+  /**
+   * Parses the config.xml file for any Urban Airship config.
+   *
+   * @param context The application context.
+   */
+  @NonNull
+  public static Map<String, String> parseConfigXml(@NonNull Context context) {
+    Map<String, String> config = new HashMap<String, String>();
+    int id = context.getResources().getIdentifier("config", "xml", context.getPackageName());
+    if (id == 0) {
+      return config;
     }
+
+    XmlResourceParser xml = context.getResources().getXml(id);
+
+    int eventType = -1;
+    while (eventType != XmlResourceParser.END_DOCUMENT) {
+
+      if (eventType == XmlResourceParser.START_TAG) {
+        if (xml.getName().equals("preference")) {
+          String name = xml.getAttributeValue(null, "name").toLowerCase(Locale.US);
+          String value = xml.getAttributeValue(null, "value");
+
+          if (name.startsWith(UA_PREFIX) && value != null) {
+            config.put(name, value);
+            PluginLogger.verbose("Found %s in config.xml with value: %s", name, value);
+          }
+        }
+      }
+
+      try {
+        eventType = xml.next();
+      } catch (Exception e) {
+        PluginLogger.error(e, "Error parsing config file");
+      }
+    }
+
+    return config;
+  }
 }
