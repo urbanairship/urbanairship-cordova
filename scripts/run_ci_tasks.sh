80c042cc37a1bb55fe2817274344b8dd89e99632
Ryan Lepinski <ryan.lepinski@urbanairship.com>
2019-04-19 15:32:26 -0700

    Release 7.5.4 (#298)
    * [MOBILE-498] Force Java6 compatibility during CI
    
    * [MOBILE-499] Provide a way to disable the google-services plugin
    
    * Release 7.5.4
    
    * Update iOS SDK to 10.2.2

--- scripts/run_ci_tasks.sh
+++ scripts/run_ci_tasks.sh
@@ -67,7 +67,7 @@ if [ "$ANDROID" = "true" ]; then
   fi
 
   # Build android
-  cordova build android -- 2>&1 | tee -a /tmp/CORDOVA-$$.out
+  cordova build android -- ---gradleArg=-PuaInternalJava6CompileOptions=true 2>&1 | tee -a /tmp/CORDOVA-$$.out
 
   # check for failures
   if grep "BUILD FAILED" /tmp/CORDOVA-$$.out; then
