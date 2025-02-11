3feb1289c537a4078d4021836fddf54009ed151b
Ryan Lepinski <ryan.lepinski@airship.com>
2025-02-11 13:27:21 -0800

    Target

--- scripts/run_ci_tasks.sh
+++ scripts/run_ci_tasks.sh
@@ -74,7 +74,7 @@ fi
 
 if [ "$IOS" = "true" ]; then
   # Build ios
-  npx cordova build ios --emulator 2>&1 | tee -a /tmp/CORDOVA-$$.out
+  npx cordova build ios --target="iPhone-16-Pro, 18.2" --emulator 2>&1 | tee -a /tmp/CORDOVA-$$.out
 
   # check for failures
   if grep "BUILD FAILED" /tmp/CORDOVA-$$.out; then
